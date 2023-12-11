da77e5b57e309cecb7a8822a9f628d41fa645b07
Farid Guzman <ghostrider_x101@msn.com>
2023-12-11 15:27:36 +0100

    added pi and sun to ulevel

--- src/shaders/frag.glsl
+++ src/shaders/frag.glsl
@@ -4,6 +4,7 @@
 // uniform mat4 viewMatrix;
 // uniform vec3 cameraPosition;
 
+float PI = 3.14159265359;
 precision highp float;
 
 // #include "../../lygia/sdf/circleSDF.glsl"
@@ -26,8 +27,15 @@ float sdBox( in vec2 p, in vec2 b )
 void main() {
 
   vec2 uv = vUv - vec2(0.5);
-  float scene = sdCircle(uv, 0.2); 
+ /*  float scene = sdCircle(uv, 0.2);  */
 /* float scene = sdBox(uv, vec2(0.2)); */
+
+
+float scene = mix(
+    sdCircle(uv, 0.2),
+    sdBox(uv, vec2(0.2)),
+    (sin(uLevel*PI*2.)+1.)*0.5
+  );
 
 float d = scene;
 
@@ -36,6 +44,7 @@ float d = scene;
     col *= 1.0 - exp(-6.0*abs(d));
 	  col *= 0.8 + 0.2*cos(150.0*d);
 	  col = mix( col, vec3(1.0), 1.0-smoothstep(0.0,0.01,abs(d)) );
+
 
   //cut into main sdf box
 if (scene < 0.0) {

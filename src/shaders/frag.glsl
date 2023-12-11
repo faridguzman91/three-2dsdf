f990446b1955572c46d1efbf6bda3cd57653412e
Farid Guzman <ghostrider_x101@msn.com>
2023-12-11 16:45:02 +0100

    added color palllete

--- src/shaders/frag.glsl
+++ src/shaders/frag.glsl
@@ -24,11 +24,23 @@ float sdBox( in vec2 p, in vec2 b )
     return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
 }
 
+vec3 pallete(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
+  return a + b*cos(6.28318*(c*t*d));
+}
+
+// pallete of colors (https://iquilezles.org/articles/palettes/)
+vec3 a = vec3(0.5, 0.5, 0.5);		
+vec3 b = vec3(0.5, 0.5, 0.5);
+vec3 c = vec3(2.0, 1.0, 0.0);
+vec3 d = vec3(0.50, 0.20, 0.25);
+
 void main() {
 
   vec2 uv = vUv - vec2(0.5);
  /*  float scene = sdCircle(uv, 0.2);  */
 /* float scene = sdBox(uv, vec2(0.2)); */
+
+/* color(t) = a + b ⋅ cos[2π(c⋅t+d)] */
 
 
 float scene = mix(
@@ -37,21 +49,28 @@ float scene = mix(
     (sin(uLevel*PI*2.)+1.)*0.5
   );
 
-float d = scene;
+float dd = scene;
 
-// coloring
-    vec3 col = (d>0.0) ? vec3(0.9,0.6,0.3) : vec3(0.65,0.85,1.0);
-    col *= 1.0 - exp(-6.0*abs(d));
-	  col *= 0.8 + 0.2*cos(150.0*d);
-	  col = mix( col, vec3(1.0), 1.0-smoothstep(0.0,0.01,abs(d)) );
+// coloring (White)
+    vec3 col = vec3(1.); 
+    //darker harsh border
+    col *= 1.0 - exp(-6.0*abs(dd*5.));
+  //black border
+	  /* col *= 0.8 + 0.2*cos(150.0*d); */
+	 /*  col = mix( col, vec3(1.0), 1.0-smoothstep(0.0,0.01,abs(d)) ); */
 
+  /* vec3 col = vec3(1.) */
+
+  vec3 color = pallete(fract(uLevel)*2., a, b, c, d);
 
   //cut into main sdf box
 if (scene < 0.0) {
     discard;
   }
 
-
+ //call color combinations
   gl_FragColor = vec4(vec3(vUv.x, vUv.y, 0.0), 1.0);
-  gl_FragColor = vec4(col, 0.2);
+  gl_FragColor = vec4(col, 1.);
+  gl_FragColor = vec4(color, 1.);
+
 }
